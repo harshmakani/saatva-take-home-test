f3918f0a60cd9428625b1448642484851ca27775
Harsh Makani <harshvardhanmakani@gmail.com>
2019-05-25 16:24:27 -0400

    Updates

---
+++ precache-manifest.49baa334b5411d2a91f2bafff91940a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8d4d02b8f64c8d3e98b868a321d199",
+    "url": "/saatva-take-home-test/index.html"
+  },
+  {
+    "revision": "cbe3e006f16b6ba354e9",
+    "url": "/saatva-take-home-test/static/css/main.514de9fe.chunk.css"
+  },
+  {
+    "revision": "0ddaf53fc9b2424ad468",
+    "url": "/saatva-take-home-test/static/js/2.559de02f.chunk.js"
+  },
+  {
+    "revision": "cbe3e006f16b6ba354e9",
+    "url": "/saatva-take-home-test/static/js/main.ceb070a2.chunk.js"
+  },
+  {
+    "revision": "be7b31f5a7d1dd6d5d0f",
+    "url": "/saatva-take-home-test/static/js/runtime~main.521676b1.js"
+  },
+  {
+    "revision": "b1eb2373c1f2bd40b6d34fa0ccc819d1",
+    "url": "/saatva-take-home-test/static/media/logo.b1eb2373.svg"
+  }
+]);
